847107aa6a704546429c40ecfa14368df080402f
tslight <tobyslight@gmail.com>
2018-05-08 22:59:29 +0100

    fix some compile warnings

--- bubble.c
+++ bubble.c
@@ -3,53 +3,52 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int printArray(int array[], int arraySize) {
-  int i;
-  for (i=0; i<arraySize; i++) {
-    if (i == arraySize-1) {
-      printf("%d\n", array[i]);
+void printArray(int array[], int arraySize) {
+    int i;
+    for (i=0; i<arraySize; i++) {
+	if (i == arraySize-1) {
+	    printf("%d\n", array[i]);
+	}
+	else {
+	    printf("%d, ", array[i]);
+	}
     }
-    else {
-      printf("%d, ", array[i]);
-    }
-  }
 }
 
-int main()
-{
-  int array[100], n, c, d, swap;
+int main() {
+    int array[100], n, c, d, swap;
 
-  printf("Enter number of elements: ");
-  scanf("%d", &n);
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
 
-  printf("Enter %d integers\n", n);
+    printf("Enter %d integers\n", n);
 
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
+    for (c = 0; c < n; c++)
+	scanf("%d", &array[c]);
 
-  bool sorted = false;
-  int lastUnsorted = n - 1;
-  int iterationCount;
-  while (!sorted) {
-    sorted = true;
-    for (c = 0; c < lastUnsorted; c++) {
-      if (array[c] > array[c+1]) /* For decreasing order use < */
-	{
-	  swap       = array[c];
-	  array[c]   = array[c+1];
-	  array[c+1] = swap;
-	  sorted = false;
+    bool sorted = false;
+    int lastUnsorted = n - 1;
+    int iterationCount;
+    while (!sorted) {
+	sorted = true;
+	for (c = 0; c < lastUnsorted; c++) {
+	    if (array[c] > array[c+1]) /* For decreasing order use < */
+	    {
+		swap       = array[c];
+		array[c]   = array[c+1];
+		array[c+1] = swap;
+		sorted = false;
+	    }
+	    printf("Array after iteration %d: ",c);
+	    printArray(array,n);
 	}
-      printf("Array after iteration %d: ",c);
-      printArray(array,n);
+	iterationCount++;
+	lastUnsorted--;
     }
-    iterationCount++;
-    lastUnsorted--;
-  }
 
-  printf("Sorted list in ascending order: ");
-  printArray(array,n);
-  printf("Iteration count: %d", iterationCount);
+    printf("Sorted list in ascending order: ");
+    printArray(array,n);
+    printf("Iteration count: %d", iterationCount);
 
-  return 0;
+    return 0;
 }
